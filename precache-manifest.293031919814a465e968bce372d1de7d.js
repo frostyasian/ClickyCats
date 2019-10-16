c91cd384e8ae0cc041616b5c1e2ac841f3c216f9
frostyasian <zphang@gmail.com>
2019-10-15 22:42:41 -0400

    Updates

---
+++ precache-manifest.293031919814a465e968bce372d1de7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c891b2908cf6869b7d8be382a1dc25e8",
+    "url": "/ClickyCats/index.html"
+  },
+  {
+    "revision": "0d27355029044352a429",
+    "url": "/ClickyCats/static/css/main.6c883aa2.chunk.css"
+  },
+  {
+    "revision": "7bcda612e7e3167e85cf",
+    "url": "/ClickyCats/static/js/2.fc710326.chunk.js"
+  },
+  {
+    "revision": "0d27355029044352a429",
+    "url": "/ClickyCats/static/js/main.35004e8a.chunk.js"
+  },
+  {
+    "revision": "a6b45badf788ab084f28",
+    "url": "/ClickyCats/static/js/runtime-main.0498f1e4.js"
+  }
+]);
